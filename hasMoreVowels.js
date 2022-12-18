035f6c23e8942acd055ffac2a40d11376b5bc0df
Mike Whaley <michael.d.whaley@gmail.com>
2022-12-18 08:54:28 -0500

    solved

---
+++ hasMoreVowels.js
@@ -0,0 +1,18 @@
+const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+let vowelCount = 0;
+//const word = "asdfjkl";
+//console.log(word.length);
+
+function hasMoreVowels(word) {
+  for (i = 0; i < word.length; i++) {
+    if (vowels.includes(word[i])) {
+      vowelCount += 1;
+    }
+  }
+  if (vowelCount > word.length / 2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(hasMoreVowels("dirtEEEEE"));
